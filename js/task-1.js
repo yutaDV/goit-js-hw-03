944cbc093cc91e1ee8a69d1ba8f2abed832c6a7f
Yuliia <74568169+yutaDV@users.noreply.github.com>
2024-04-21 23:25:27 +0300

    first commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,12 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    let totalPrice = quantity * pricePerDroid;
-    if (customerCredits >= totalPrice) { 
-        return `You ordered ${quantity}  droids worth ${totalPrice} credits!`
-    }
-    else { 
-        return "Insufficient funds!"
-    }
-
-    
+function slugify(title) {
+    const words = title.split(' ');
+    const slug = words.map(word => word.toLowerCase()).join('-');
+    return slug;
 };
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
